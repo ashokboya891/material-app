98ba5b280dfd9e8fd8be9ae33d7dd40b86917ae6
AshokKumar <ashokboya891@gmail.com>
2025-01-30 16:11:36 +0530

    shared module,booking component with material packages

--- material-app/src/app/app.module.ts
+++ material-app/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule  } from "./material/material.module";
+import { BookingComponent } from './Components/booking/booking.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
